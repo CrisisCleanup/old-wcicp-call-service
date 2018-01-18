d6b28ad2c1dda2baf237d27062ddf975841bef2f
Kira Jones <kirajones@KJONES-MBP-HOU.local>
2018-01-18 16:53:30 -0600

    Adding detailed view for retrieving the training's related questions

--- crisiscleanup/calls/api/trainingModule.py
+++ crisiscleanup/calls/api/trainingModule.py
@@ -1,10 +1,13 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, filters, status
 from rest_framework.decorators import list_route
+from rest_framework.decorators import detail_route
 from rest_framework.response import Response
 
 from crisiscleanup.calls.api.serializers.trainingModule import TrainingModuleSerializer
+from crisiscleanup.calls.api.serializers.trainingQuestion import TrainingQuestionSerializer
 from crisiscleanup.calls.models import TrainingModule
+from crisiscleanup.calls.models import TrainingQuestion
 from crisiscleanup.taskapp.celery import debug_task
 
 
@@ -21,3 +24,11 @@ class TrainingModuleViewSet(viewsets.ModelViewSet):
             'task_id': debug_task.delay().id
         }
         return Response(resp, status=status.HTTP_200_OK)
+
+    @detail_route(methods=['get'])
+    def get_detail(self, request, pk=None):
+        trainingModule = self.get_object()
+        serializedData = self.get_serializer(trainingModule).data;
+        questions = TrainingQuestion.objects.filter(module=trainingModule)
+        serializedData["questions"] = TrainingQuestionSerializer(questions, many=True).data;
+        return Response(serializedData)
