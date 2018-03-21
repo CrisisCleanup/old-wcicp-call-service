b10fb56bd7ef7b40b083a3c537658b88bd7172f0
Tyler Sanders <tylerssanders@gmail.com>
2018-03-20 22:42:44 -0500

    Add migration script

--- compose/kube/django/gunicorn.sh
+++ compose/kube/django/gunicorn.sh
@@ -7,4 +7,5 @@ set -o nounset
 echo "Executing gunicorn"
 
 python /app/manage.py collectstatic --noinput
+python /app/manage.py migrate --noinput
 /usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:5000 --chdir=/app
